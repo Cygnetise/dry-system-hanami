1af44a313f4653b14e1e83fd6bf639f1928fdfc6
David Dawson <david.dawson@gmail.com>
2019-09-13 15:08:25 +0100

    Fix the regexpr that is used to ignore files

--- lib/dry/system/hanami/resolver.rb
+++ lib/dry/system/hanami/resolver.rb
@@ -10,7 +10,7 @@ module Dry
         DEFAULT_RESOLVER = ->(k) { k.new }
 
         def register_folder!(folder, resolver: DEFAULT_RESOLVER, ignore: [], memoize: false)
-          regexp = ignore.any? ? Regexp.new(/(#{ignore.join("|")})$/) : nil
+          regexp = ignore.any? ? Regexp.new(/^#{LIB_FOLDER}#{folder}\/(#{ignore.join("|")})/) : nil
           all_files_in_folder(folder).each do |file|
             next if regexp && file.match?(regexp)
 
@@ -33,7 +33,7 @@ module Dry
 
         def all_files_in_folder(folder)
           Dir.chdir(::Hanami.root) do
-            Dir.glob("lib/#{folder}/**/*.rb")
+            Dir.glob("#{LIB_FOLDER}#{folder}/**/*.rb")
                .map! { |file_name| file_name.sub('.rb', '').to_s }
           end
         end
